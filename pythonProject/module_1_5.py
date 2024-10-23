532d305a35002f4814ddf1b6885c93cd192f0bc4
Rivalv3 <167709224+Rivalv3@users.noreply.github.com>
2024-10-24 00:07:26 +0500

    Add files via upload

---
+++ pythonProject/module_1_5.py
@@ -0,0 +1,6 @@
+immutable_var = 2, "string", True, 4.55
+print(immutable_var)
+# immutable_var[3] = False  Кортежы яв-ся неизменяемыми
+mutable_list = ["Urban", 3, 4, False]
+mutable_list[0] = "University"
+print(mutable_list)
